a7f0ba44dbd541abf18ad1f767770b61b9fe9589
mateus <mateus.03fernandesaraujo@gmail.com>
2025-08-31 17:58:23 -0300

    feat: connected to db

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 import express from "express"
+import mysql from "mysql"
 const app = express()
 const port = process.env.PORT || 3002
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "new_password",
+  database: "meu_app"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting MySQL:", err);
+    return;
+  }
+  console.log("COnnected MySQL!");
+});
+
 app.get('/', (req, res) => {
-    res.send("Oi, teste")
+    db.query("SELECT * FROM usuarios", (err, results) => {
+    if (err) {
+      console.error("Query error:", err);
+      return res.status(500).send("User error");
+    }
+    res.json(results);
+  });
 })
 
 app.listen(port, () => {
